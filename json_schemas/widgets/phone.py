ea55f2713f42116ba264cf778ff4f068b9a776c1
Keith <keith@tauri-tec.com>
2023-09-28 21:27:29 +0100

    Correcting the input type for the phone widget

--- json_schemas/widgets/phone.py
+++ json_schemas/widgets/phone.py
@@ -3,5 +3,5 @@ from json_schemas.widgets.base import BaseWidget
 
 class JSONPhoneWidget(BaseWidget):
 
-    default_input_type: str = "phone"
+    default_input_type: str = "tel"
     default_input_attributes: list[str] = ["placeholder"]
